f2f24c5e8a0c6f9b8fe6694833b98358055c9b02
Michał Kuś <kusmichal06@gmail.com>
2021-01-11 21:20:46 +0100

    Coded tests

--- TDD/test.cpp
+++ TDD/test.cpp
@@ -1,7 +1,60 @@
 #include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+template<typename T>
+class CBuffer
+{
+	
+};
+
+class BufferTest : public ::testing::Test
+{
+public:
+	CBuffer<int> cb;
+
+	void SetUp() override
+	{
+		cb = CBuffer<int>(10);
+		int testValues[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+		for (int& val : testValues)
+		{
+			cb.add(val);
+		}
+	}
+
+	void TearDown() override
+	{
+
+	}
+};
+
+TEST_F(BufferTest, ConstructorTest) {
+  ASSERT_EQ(10, cb.getAllocatedSize());
+}
+
+TEST_F(BufferTest, SizeTest) {
+	cb.get();
+	cb.get();
+	ASSERT_EQ(8, cb.getSize());
+}
+
+TEST_F(BufferTest, AddTest) {
+	cb.add(2137);
+	for (int i = 0; i < 9; i++)
+	{
+		cb.get();
+	}
+	ASSERT_EQ(2137, cb.get());
+}
+
+TEST_F(BufferTest, GetTest) {
+	ASSERT_EQ(1, cb.get());
+}
+
+TEST_F(BufferTest, GetEmptyTest) {
+	for (int i = 0; i < 10; i++)
+	{
+		cb.get();
+	}
+	ASSERT_THROW(cb.get(), std::exception);
 }
